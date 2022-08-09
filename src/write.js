06afe9d6165fe21848738713d8b403884e19faf5
JunHoe Heo <jhjjang0902@gmail.com>
2022-08-10 03:45:51 +0900

    불필요한 기능 제거

--- src/write.js
+++ src/write.js
@@ -6,13 +6,6 @@ function showcontent (event) {
     const content = contentBox.value;
     const showContent = document.querySelector(".show_content");
     showContent.innerText = content;
-    show ();
 }
 
-function show () {
-    const main = document.querySelector(".container");
-    const show = document.querySelector(".show");
-    main.classList.toggle("hidden");
-    show.classList.toggle("hidden");
-}
 enroll.addEventListener("click", showcontent);
