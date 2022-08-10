22da048307aba02906ecffe982ed79162316ee2b
JunHoe Heo <jhjjang0902@gmail.com>
2022-08-11 02:14:56 +0900

    id 제목 내용 아래에 표시하기

--- src/write.js
+++ src/write.js
@@ -3,9 +3,22 @@ const enroll = document.querySelector("button");
 function showcontent (event) {
     event.preventDefault();
     const contentBox = document.querySelector(".article");
-    const content = contentBox.value;
-    const showContent = document.querySelector(".show_content");
-    showContent.innerText = content;
+    const id = document.querySelector(".id"); 
+    const title = document.querySelector(".title");
+
+    const  articleZone = document.createElement("div");
+    const titleZone = document.createElement("h2");
+    const idZone = document.createElement("span");
+
+    titleZone.innerText = title.value;
+    idZone.innerText = `작성자: ${id.value}`;
+    articleZone.innerText = contentBox.value; 
+
+    const showContent = document.querySelector(".show");
+
+    showContent.appendChild(titleZone);
+    showContent.appendChild(idZone);
+    showContent.appendChild(articleZone);
 }
 
 enroll.addEventListener("click", showcontent);
