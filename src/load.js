2a5335a85fe5e791c50d805ffcbd007572ef72a1
JunHoe Heo <jhjjang0902@gmail.com>
2022-08-13 02:59:57 +0900

    글목록 페이지 추가에 따른 로직 변경

--- src/load.js
+++ src/load.js
@@ -1,4 +1,4 @@
-const inform = localStorage.getItem("article");
+const inform = localStorage.getItem("show");
 const parsedInform = JSON.parse(inform);
 
 function showContent (informs) {
@@ -10,8 +10,6 @@ function showContent (informs) {
     idZone.innerText = `작성자: ${informs.id}`;
     titleZone.innerText = informs.title;
     articleZone.innerText = informs.content;
-
-    console.log(informs.title);
 
     show.appendChild(titleZone);
     show.appendChild(idZone);
