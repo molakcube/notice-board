5b59a4d134641c723604f64fb68040be7b06b1d7
JunHoe Heo <jhjjang0902@gmail.com>
2022-08-13 02:58:19 +0900

    글목록 페이지에 대한 스크립트 추가

---
+++ src/list.js
@@ -0,0 +1,36 @@
+const inform = localStorage.getItem("article");
+const list = document.getElementById("list");
+const parsedInform = JSON.parse(inform);
+
+function showPage(event) {
+    const target = event.target.parentElement.parentElement;
+    const show = parsedInform.filter((showP) => showP.title == target.id);
+    localStorage.setItem("show", JSON.stringify(show));
+};
+
+function showContent (informs) {
+    const articleBox = document.createElement("div");
+    articleBox.id = informs.title;
+    const titleZone = document.createElement("h2");
+    const idZone = document.createElement("span");
+    const dateZone = document.createElement("span");
+
+    const loadLink = document.createElement("a");
+    loadLink.setAttribute("href","noticeboard.html");
+
+    idZone.innerText = `작성자: ${informs.id}`;
+    titleZone.innerText = informs.title;
+    dateZone.innerText = informs.date;
+
+
+    loadLink.appendChild(titleZone);
+    articleBox.appendChild(loadLink);
+
+    articleBox.appendChild(idZone);
+    articleBox.appendChild(dateZone);
+    list.appendChild(articleBox);
+
+    loadLink.addEventListener("click", showPage);
+}
+
+parsedInform.forEach(showContent);
